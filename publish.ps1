60d160cd8c483ac02c9c92ffad46e2685b30903a
Alex Lein <alein@trakitwireless.com>
2026-06-11 13:03:37 -0400

    Environment

---
+++ publish.ps1
@@ -0,0 +1,43 @@
+# Copy package.json, README.md, and LICENSE.md to dist, then publish from dist
+# Usage: powershell -ExecutionPolicy Bypass -File publish.ps1
+
+$root = Split-Path -Parent $MyInvocation.MyCommand.Definition
+$publishDir = Join-Path $root 'dist'
+$indexFile = Join-Path $root 'src\index.ts'
+$packageFile = Join-Path $root 'package.json'
+
+# Increment version in package.json and update index.ts
+if ((Test-Path $packageFile) -and (Test-Path $indexFile)) {
+	$packageContent = Get-Content $packageFile
+	$regex = [regex]'"version":\s*"(-?\d+)\.(-?\d+)\.(-?\d+)"'
+	$match = $regex.Match($packageContent)
+	if ($match.Success) {
+		$major = [int]$match.Groups[1].Value
+		$minor = [int]$match.Groups[2].Value
+		$patch = [int]$match.Groups[3].Value + 1
+		$newVer = "$major.$minor.$patch"
+		$pkgVer = '"version": "' + $newVer + '"'
+		$newPackageContent = $packageContent -replace '"version":\s*"[^"]+"', $pkgVer
+		# Write updated package.json
+		Set-Content $packageFile $newPackageContent
+		# Update index.ts with new version
+		$indexContent = Get-Content $indexFile
+		$newIndexContent = $indexContent -replace 'export const version = [^;]+;', "export const version = '$newVer';"
+		Set-Content $indexFile $newIndexContent
+	} else {
+		Write-Host "Version format in package.json is invalid. Skipping version bump."
+	}
+}
+
+# Build rollup package
+npx rollup --config rollup.config.js
+
+# Copy package.json, README.md, LICENSE if they exist
+Copy-Item "$packageFile" "$publishDir\package.json" -Force
+if (Test-Path "$root\README.md") { Copy-Item "$root\README.md" "$publishDir\README.md" -Force }
+if (Test-Path "$root\LICENSE") { Copy-Item "$root\LICENSE" "$publishDir\LICENSE" -Force }
+
+# Change to dist directory and publish
+Push-Location $publishDir
+npm publish
+Pop-Location
---
+++ rollup.config.js
@@ -0,0 +1,54 @@
+import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+//import json from '@rollup/plugin-json';
+
+const obfuscate = {
+	ecma: 2020,
+	// sourceMap: {
+	//   filename: 'dist/trakit-sync-worker.min.js',
+	// },
+	compress: {
+		drop_console: !true,
+		drop_debugger: !true,
+		hoist_funs: true,
+		module: true,
+		toplevel: true,
+	},
+	mangle: {
+		properties: {
+			regex: /^[#_]/,
+		}
+	}
+	//format: {
+	//	semicolons: false,
+	//},
+};
+
+export default [
+	{
+		input: './src/index.ts',
+		output: [
+			{
+				file: 'dist/trakit-sync.min.js',
+				format: 'es',
+				exports: 'named',
+				plugins: [terser(obfuscate)]
+			}
+		],
+		plugins: [
+			//json(),
+			typescript({
+				tsconfig: './tsconfig.json',
+				//tsconfigOverride: {
+				//	compilerOptions: {
+				//		declaration: false,
+				//	}
+				//}
+			})
+		],
+		external: [
+			'@trakit/objects',
+			'@trakit/commands',
+		],
+	}
+];
