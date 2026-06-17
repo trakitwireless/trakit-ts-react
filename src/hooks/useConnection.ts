321825168ef1dc948ff364cfb6ae0ea68daabbb9
Alex Lein <alein@trakitwireless.com>
2026-06-17 14:27:17 -0400

    This hook can control the cookies directly

--- src/hooks/useConnection.ts
+++ src/hooks/useConnection.ts
@@ -1,6 +1,4 @@
-import {
-    RepSelfGet,
-} from '@trakit/commands';
+import { RepSelfGet } from '@trakit/commands';
 import {
     guid,
     Machine,
@@ -16,11 +14,35 @@ import {
     TrakitSocketCommander,
     TrakitSyncCommander,
 } from '@trakit/sync';
-import {
-    useEffect,
-    useRef,
-    useState,
-} from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { useCookies } from 'react-cookie';
+import { CookieSetOptions } from 'universal-cookie';
+
+/**
+ * Name of the session ID cookie.
+ * This cookie is used to persist the user's session across page reloads and browser restarts.
+ */
+const SESSION_ID = "ghostId";
+/**
+ * Name of the machine key cookie.
+ * This cookie is used to persist the machine's key across page reloads and browser restarts.
+ */
+const MACHINE_KEY = "apiKey";
+/**
+ * Name of the machine secret cookie.
+ * This cookie is used to persist the machine's secret across page reloads and browser restarts.
+ */
+const MACHINE_SECRET = "apiSec";
+/**
+ * Default options for setting cookies.
+ * These options ensure that cookies are only accessible via the client-side and are secure.
+ */
+const COOKIE_OPTIONS: CookieSetOptions = {
+	path: "/",
+	secure: true,
+	sameSite: 'strict',
+	expires: new Date(0),	// default to expired; should be overridden with actual expiry when setting cookies
+};
 
 /**
  * The result of the `useConnection` hook, providing the synchronizer instance and connection state.
@@ -55,6 +77,7 @@ export type UseConnectionResult = {
 	machine: Machine | null;
 };
 
+
 /**
  * A React hook that manages the connection to the Trak-iT synchronization service.
  * It initializes the synchronizer, listens for connection and account events,
@@ -74,13 +97,17 @@ export default function useConnection(
 	));
 
 	/**
+	 * The cookies for session ID, {@link Machine.key}, and {@link Machine.secret}.
+	 */
+	const [cookies, setCookie, removeCookie] = useCookies([SESSION_ID, MACHINE_KEY, MACHINE_SECRET]);
+	/**
 	 * Indicates whether the WebSocket connection is currently online.
 	 */
 	const [online, setOnline] = useState(s.current.socketOnline);
 	/**
 	 * The session ID taken from a login command.
 	 */
-	const [ghostId, setGhostId] = useState<guid>(s.current.account.ghostId || "");
+	const [ghostId, setGhostId] = useState<guid>(cookies[SESSION_ID] || "");
 	/**
 	 * The {@link User} information associated with the current session, if available.
 	 */
@@ -88,7 +115,14 @@ export default function useConnection(
 	/**
 	 * The {@link Machine|API credentials} associated with the current session, if available.
 	 */
-	const [machine, setMachine] = useState<Machine | null>(s.current.account.machine || null);
+	const [machine, setMachine] = useState<Machine | null>(
+		s.current.account.machine
+		|| cookies[MACHINE_KEY] && new Machine({
+			key: cookies[MACHINE_KEY],
+			secret: cookies[MACHINE_SECRET],
+		})
+		|| null
+	);
 	/**
 	 * Indicates whether the connection is ready for use.
 	 */
@@ -106,14 +140,33 @@ export default function useConnection(
 	 * @param event
 	 */
 	function handleAccount(event: TrakitEvent) {
-		const { account: a } = event as TrakitEventAccount;
-		setGhostId(
-			a.user?.login && a.expiry > new Date
-				? a.ghostId
-				: ""
-		);
-		setUser(a.user || null);
-		setMachine(a.machine || null);
+		const { account } = event as TrakitEventAccount;
+
+		if (account.user?.login && account.expiry > new Date) {
+			setCookie(SESSION_ID, account.ghostId, {
+				...COOKIE_OPTIONS,
+				expires: account.expiry,
+			});
+			setGhostId(account.ghostId || "");
+		} else {
+			removeCookie(SESSION_ID, COOKIE_OPTIONS);
+		}
+		setUser(account.user || null);
+
+		if (account.machine?.key && account.expiry > new Date) {
+			setCookie(MACHINE_KEY, account.machine.key, {
+				...COOKIE_OPTIONS,
+				expires: account.expiry,
+			});
+			setCookie(MACHINE_SECRET, account.machine.secret, {
+				...COOKIE_OPTIONS,
+				expires: account.expiry,
+			});
+		} else {
+			removeCookie(MACHINE_KEY, COOKIE_OPTIONS);
+			removeCookie(MACHINE_SECRET, COOKIE_OPTIONS);
+		}
+		setMachine(account.machine || null);
 	}
 
 	/**
@@ -125,13 +178,13 @@ export default function useConnection(
 		s.current.on("error", handleOnline);
 		s.current.on("close", handleOnline);
 		s.current.on("account", handleAccount);
-		return () => {
-			s.current.off("open", handleOnline);
-			s.current.off("error", handleOnline);
-			s.current.off("close", handleOnline);
-			s.current.off("account", handleAccount);
-			s.current.dispose();
-		};
+		if (ghostId || machine) {
+			s.current.setAuth({
+				ghostId,
+				machine: machine?.toJSON(),
+			});
+		}
+		return () => s.current.dispose();
 	}, []);
 
 	return {
