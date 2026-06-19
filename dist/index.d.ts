e52a8d6a3bf08c2f4b823cc7c8aea7cc547c9103
Alex Lein <alein@trakitwireless.com>
2026-06-19 16:04:27 -0400

    Rebuild and published

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -10,7 +10,7 @@ import { useSingle, useSync, UseSyncMultiple, UseSyncResult, UseSyncSingle } fro
 /**
  * Version number for this release.
  */
-export declare const version = "0.0.4";
+export declare const version = "0.0.5";
 /**
  * Hooks
  */
